6b07db307c90bd90fd75a9626e34eacaf9a53165
AllisterF1 <allytheforbes@gmail.com>
2023-05-03 16:43:04 +0100

    login fetch and comp setup

---
+++ src/utils/index.js
@@ -0,0 +1,22 @@
+
+
+
+export const loginUser = async (username, password, newUser) => {
+    try {
+      const response = await fetch("http://localhost:5001/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      newUser(data.user.username);
+    } catch (error) {
+      console.log(error);
+    }
+  };
