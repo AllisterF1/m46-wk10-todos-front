6ffbb37ea05ca8ccfbb2af402c90a3f3dd954bdf
AllisterF1 <allytheforbes@gmail.com>
2023-05-04 10:56:35 +0100

    formatted

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,46 +1,41 @@
-
-
 // login the user to the app
 export const loginUser = async (username, password, newUser) => {
-    try {
-      const response = await fetch("http://localhost:5001/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: username,
-          password: password,
-        }),
-      });
-      const data = await response.json();
-      console.log(data);
-      newUser(data.user.username);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const response = await fetch("http://localhost:5001/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    newUser(data.user.username);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  // add an active todo to db - needs token auth adding later - placeholder of jwtToken 
-  export const addtodo = async (jwtToken) => {
-    
-  
-    try {
-      const response = await fetch("http://localhost:5001/activetodos/addtodo", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${jwtToken}`,
-        },
-        body: JSON.stringify({
-          todo: todo
-         
-        }),
-      });
-      const data = await response.json();
-        
-      return data;
-    } catch (error) {
-      return({errorMessage: error.message, error:error});
-    }
-  };
+// add an active todo to db - needs token auth adding later - placeholder of jwtToken
+export const addtodo = async (jwtToken) => {
+  try {
+    const response = await fetch("http://localhost:5001/activetodos/addtodo", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      body: JSON.stringify({
+        todo: todo,
+      }),
+    });
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    return { errorMessage: error.message, error: error };
+  }
+};
