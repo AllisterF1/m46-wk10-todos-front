743a0ce1d07660ab30b33c2d7bba5876a4c02084
AllisterF1 <allytheforbes@gmail.com>
2023-05-04 10:55:53 +0100

    login fetch setup

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 
 
-
+// login the user to the app
 export const loginUser = async (username, password, newUser) => {
     try {
       const response = await fetch("http://localhost:5001/users/login", {
@@ -18,5 +18,29 @@ export const loginUser = async (username, password, newUser) => {
       newUser(data.user.username);
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  // add an active todo to db - needs token auth adding later - placeholder of jwtToken 
+  export const addtodo = async (jwtToken) => {
+    
+  
+    try {
+      const response = await fetch("http://localhost:5001/activetodos/addtodo", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${jwtToken}`,
+        },
+        body: JSON.stringify({
+          todo: todo
+         
+        }),
+      });
+      const data = await response.json();
+        
+      return data;
+    } catch (error) {
+      return({errorMessage: error.message, error:error});
     }
   };
