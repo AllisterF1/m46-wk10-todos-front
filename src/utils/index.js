bccbf971d9a373ddf128d42c13d336e8166e29d3
Stephen-M5321 <127414937+Stephen-M5321@users.noreply.github.com>
2023-05-04 11:05:05 +0100

    index update register added

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,23 @@
 
-
+export const registerUser = async (username, email, password) => {
+  try {
+      const response = await fetch("http://localhost:5002/users/register", {
+          method: "POST",
+          headers: {
+              "Content-Type" : "application/json",
+          },
+          body: JSON.stringify({
+              "username" : username,
+              "email" : email,
+              "password": password
+          })
+      })
+      const data = await response.json()
+      console.log(data)
+  } catch (error) {
+      console.log(error)
+  }
+}
 
 export const loginUser = async (username, password, newUser) => {
     try {
